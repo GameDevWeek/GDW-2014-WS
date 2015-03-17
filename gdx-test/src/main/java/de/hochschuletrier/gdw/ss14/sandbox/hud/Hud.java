8a8c29c0dafa219b6ce6a10ad50a94f8285da5e5
Julia <juliaeiden@yahoo.de>
2015-03-17 10:21:29 +0100

    HudKlasse

---
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/hud/Hud.java
@@ -0,0 +1,17 @@
+package de.hochschuletrier.gdw.ss14.sandbox.hud;
+
+import com.badlogic.gdx.scenes.scene2d.Group;
+
+import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
+import de.hochschuletrier.gdw.ss14.sandbox.SandboxGame;
+
+public class Hud extends Group
+{
+    
+    public Hud()
+    {
+        
+    }
+
+
+}
