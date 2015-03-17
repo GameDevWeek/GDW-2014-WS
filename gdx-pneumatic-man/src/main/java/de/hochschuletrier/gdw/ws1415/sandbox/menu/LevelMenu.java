3d17f6e468776bf12fc5e4cfcd1322cd40a8ac4f
meroremo <mero-yui@hotmail.de>
2015-03-17 11:14:50 +0100

    Added dummy menu_background image to source and slightly updated OptionMenu class

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/menu/LevelMenu.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/menu/LevelMenu.java
@@ -1,5 +1,13 @@
 package de.hochschuletrier.gdw.ws1415.sandbox.menu;
 
-public class LevelMenu {
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
 
+public class LevelMenu extends MenuPage 
+{
+	
+	public LevelMenu(Skin skin, MenuManager menuManager)
+	{
+		super(skin, "menu_background");
+	}
 }
--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/menu/OptionMenu.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/menu/OptionMenu.java
@@ -1,6 +1,18 @@
 package de.hochschuletrier.gdw.ws1415.sandbox.menu;
 
-public class OptionMenu 
-{
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
+import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
+
+public class OptionMenu extends MenuPage
+{
+	//private final TextButton mainMenuButton;
+	
+	public OptionMenu(Skin skin, MenuManager menuManager)
+	{
+		super(skin, "background_menu");
+		
+		addCenteredButton(menuManager.getWidth() - 150, 50, 150, 50, "RETURN", () -> menuManager.popPage());
+	}
 }
