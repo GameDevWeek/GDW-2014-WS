9077d5b06cd9c6e91642092a7035420ea1cb7d89
Jeremy <jeremy1993@freenet.de>
2015-03-17 14:16:53 +0100

    Ingame Hud test

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/hud/Hud.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/hud/Hud.java
@@ -1,6 +1,12 @@
 package de.hochschuletrier.gdw.ss14.sandbox.hud;
 
-import com.badlogic.gdx.scenes.scene2d.Group;
+import org.lwjgl.opengl.GL11;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
 import de.hochschuletrier.gdw.ss14.sandbox.SandboxGame;
@@ -10,20 +16,76 @@ import de.hochschuletrier.gdw.ss14.sandbox.SandboxGame;
 public class Hud extends SandboxGame
 {
     
+  private int allMiners;
+  
+  private SpriteBatch batch;
+  private Texture texture;
+  private Sprite sprite;
+  private Stage stage;
+  
+      @Override
+      public void init(AssetManagerX assetManager)
+      {
+          batch = new SpriteBatch();
+          texture = new Texture(Gdx.files.internal("data/menuImages/dummyMiner.png"));
+          sprite = new Sprite(texture);
+          stage = new Stage();
+         // allMiners = getMinersLeft();
+          allMiners = 5;
+          //loadMinersPictures();
+        
+          
+      }
+    
+
+    @Override
+    public void update(float delta)
+    {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL11.GL_COLOR_BUFFER_BIT);
+   
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
+        
+    }
+    
+    @Override
+    public void dispose()
+    {
+        batch.dispose();
+        texture.dispose();
+        
+    }
+    
+ /*   
+    
+    protected Main main = Main.getInstance();
+    protected AssetManagerX assetManager;
+//    protected final Skin skin;
+    
+    
+    
     private int allMiners;
+    
+    private SpriteBatch batch;
+    private Texture texture;
+    private Sprite sprite;
+    
+//    private Stage stage = new Stage();
     
     public Hud()
     {
 //        allMiners = getMinersLeft();
         allMiners = 5;
-        
+        loadMinersPictures();
         
     }
     
     
-    
     public void loadMinersPictures()
     {
+
         
         while (allMiners > 0 )
         {
@@ -31,7 +93,9 @@ public class Hud extends SandboxGame
             
         }
         
+        batch = new SpriteBatch();
         
+        sprite = new Sprite(texture);
         
         
     }
@@ -41,8 +105,18 @@ public class Hud extends SandboxGame
     @Override
     public void dispose()
     {
-        // TODO Auto-generated method stub
+        batch.dispose();
+        texture.dispose();
         
+    }
+    
+    public void render() {        
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL11.GL_COLOR_BUFFER_BIT);
+        
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
     }
 
 
@@ -50,7 +124,8 @@ public class Hud extends SandboxGame
     @Override
     public void init(AssetManagerX assetManager)
     {
-        // TODO Auto-generated method stub
+        this.assetManager = assetManager;
+        
         
     }
 
@@ -62,6 +137,37 @@ public class Hud extends SandboxGame
         // TODO Auto-generated method stub
         
     }
-
+    
+ */   
+    
+    
+/*
+    
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "LibGDX Test";
+        cfg.width = 1920;
+        cfg.height = 1080;
+        cfg.useGL30 = false;
+        cfg.vSyncEnabled = true;
+        cfg.foregroundFPS = 60;
+        cfg.backgroundFPS = 60;
+        
+        
+//        initApplication();
+        
+        new LwjglApplication(getInstance(), cfg);
+    }
+    
+    private static Main instance;
+    
+    public static Main getInstance() {
+        if (instance == null) {
+            instance = new Main(); // hello
+        }
+        return instance;
+    }
+    
+*/
 
 }
