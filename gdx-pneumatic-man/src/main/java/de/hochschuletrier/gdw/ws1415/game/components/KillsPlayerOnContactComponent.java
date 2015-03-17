ecbae7730eca138f7d7e9109e8771022e0deec42
Skidbeast <tobias.thierbach@gmail.com>
2015-03-17 11:12:27 +0100

    ->added Pool.Poolable

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/KillsPlayerOnContactComponent.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/KillsPlayerOnContactComponent.java
@@ -1,11 +1,19 @@
 package de.hochschuletrier.gdw.ws1415.game.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool;
 
 /**
  * Created by oliver on 16.03.15.
  *
  * Used for Collision.
  */
-public class KillsPlayerOnContactComponent extends Component {
+public class KillsPlayerOnContactComponent extends Component implements
+        Pool.Poolable {
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+
+    }
 }
