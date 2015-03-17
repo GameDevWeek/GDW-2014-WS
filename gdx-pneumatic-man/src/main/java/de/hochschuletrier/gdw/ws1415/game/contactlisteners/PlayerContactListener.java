04a369a05313916477aee81e35daa40f22288872
Sinan <sinan.sagunc@gmail.com>
2015-03-17 11:28:51 +0100

    Added condition for the damage that the enemy does to the player.

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/contactlisteners/PlayerContactListener.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/contactlisteners/PlayerContactListener.java
@@ -1,11 +1,12 @@
 package de.hochschuletrier.gdw.ws1415.game.contactlisteners;
 
 import com.badlogic.ashley.core.Entity;
+
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixContact;
 import de.hochschuletrier.gdw.commons.gdx.physix.PhysixContactAdapter;
-import de.hochschuletrier.gdw.commons.gdx.physix.components.PhysixBodyComponent;
 import de.hochschuletrier.gdw.ws1415.game.ComponentMappers;
-import de.hochschuletrier.gdw.ws1415.game.components.TriggerComponent;
+import de.hochschuletrier.gdw.ws1415.game.components.DamageComponent;
+import de.hochschuletrier.gdw.ws1415.game.components.HealthComponent;
 
 /**
  * Handles contacts between player and other entities
@@ -13,14 +14,23 @@ import de.hochschuletrier.gdw.ws1415.game.components.TriggerComponent;
 public class PlayerContactListener extends PhysixContactAdapter {
 
     public void beginContact(PhysixContact contact) {
-        if(contact.getOtherComponent() == null) return;
+
+        Entity player = contact.getMyComponent().getEntity();
+
+        if (contact.getOtherComponent() == null)
+            return;
 
         // Entity myEntity = contact.getMyComponent().getEntity(); //
         Entity otherEntity = contact.getOtherComponent().getEntity();
-        if(ComponentMappers.enemy.has(otherEntity)){
+        if (ComponentMappers.enemy.has(otherEntity)) {
             // player touched an enemy
+
+            if (otherEntity.getComponent(DamageComponent.class).damageToPlayer) {
+                // Damage system has to reduce the players health by the amount
+                // specified in the damage component here.
+            }
+            player.getComponent(HealthComponent.class);
         }
     }
-
 
 }
