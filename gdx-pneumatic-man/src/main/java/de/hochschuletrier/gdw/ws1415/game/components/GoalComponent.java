f6ad82211030893ea9da8902f8849b3dae85a86b
Sinan <sinan.sagunc@gmail.com>
2015-03-17 15:58:57 +0100

    Removed commentaries.

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/GoalComponent.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/GoalComponent.java
@@ -5,11 +5,6 @@ import com.badlogic.gdx.utils.Pool;
 
 public class GoalComponent extends Component implements Pool.Poolable {
 
-    // Die Anzahl an Kollegen die man noch retten muss um das Levelziel zu
-    // erreichen. Für jeden geretteten Kollegen
-    // wird die Variable um 1 reduziert. Wenn keine Kollegen mehr gerettet
-    // werden müssen (die Variable also 0 erreicht)
-    // hat man das Level geschafft.
     public int mineworkers_saved;
     public int number_of_mineworkers_total;
     public boolean end_of_level;
