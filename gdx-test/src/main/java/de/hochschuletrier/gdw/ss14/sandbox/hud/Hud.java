6396bb37d022270a3c932a2d4572a32dc39003c9
Julia <juliaeiden@yahoo.de>
2015-03-17 10:42:57 +0100

    HudClass erweitert

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/hud/Hud.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/hud/Hud.java
@@ -2,14 +2,19 @@ package de.hochschuletrier.gdw.ss14.sandbox.hud;
 
 import com.badlogic.gdx.scenes.scene2d.Group;
 
-import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
-import de.hochschuletrier.gdw.ss14.sandbox.SandboxGame;
+
 
 public class Hud extends Group
 {
     
     public Hud()
     {
+        
+    }
+    
+    public void loadMinersPictures()
+    {
+        
         
     }
 
