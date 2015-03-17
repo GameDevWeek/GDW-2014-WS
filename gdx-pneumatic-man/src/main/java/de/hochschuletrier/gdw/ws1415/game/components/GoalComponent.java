5cb30114394414ba0327a262a876c42f9a533cc7
Sinan <sinan.sagunc@gmail.com>
2015-03-17 11:05:56 +0100

    inserted spaces for tabs.

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/GoalComponent.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/components/GoalComponent.java
@@ -3,16 +3,18 @@ package de.hochschuletrier.gdw.ws1415.game.components;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.utils.Pool;
 
-public class GoalComponent extends Component implements Pool.Poolable{
-	
-	//Die Anzahl an Kollegen die man noch retten muss um das Levelziel zu erreichen. Für jeden geretteten Kollegen
-	//wird die Variable um 1 reduziert. Wenn keine Kollegen mehr gerettet werden müssen (die Variable also 0 erreicht)
-	//hat man das Level geschafft.
-	public int unsaved_colleagues;
+public class GoalComponent extends Component implements Pool.Poolable {
 
-	@Override
-	public void reset() {
-		unsaved_colleagues = 0;
-	}
-	
+    // Die Anzahl an Kollegen die man noch retten muss um das Levelziel zu
+    // erreichen. Für jeden geretteten Kollegen
+    // wird die Variable um 1 reduziert. Wenn keine Kollegen mehr gerettet
+    // werden müssen (die Variable also 0 erreicht)
+    // hat man das Level geschafft.
+    public int unsaved_colleagues;
+
+    @Override
+    public void reset() {
+        unsaved_colleagues = 0;
+    }
+
 }
