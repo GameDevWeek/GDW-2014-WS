af241095cbe5822d69f2e669254f1316dc9b687e
meroremo <mero-yui@hotmail.de>
2015-03-17 11:28:19 +0100

    Added beenden_button image and updated background_menu image. Also added empty CreditsMenu class to menu-package

---
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/menu/CreditsMenu.java
@@ -0,0 +1,13 @@
+package de.hochschuletrier.gdw.ws1415.sandbox.menu;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
+
+public class CreditsMenu extends MenuPage 
+{
+
+	public CreditsMenu(Skin skin, MenuManager menuManager) {
+		super(skin, "menu_background");
+	}
+
+}
