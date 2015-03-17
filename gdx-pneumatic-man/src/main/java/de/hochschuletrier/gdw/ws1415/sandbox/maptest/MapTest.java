56060eb9b0e7aed8fdd53a58c9c46cc152e24127
Hermann <H.Hermann94@googlemail.com>
2015-03-17 10:48:43 +0100

    insertet spaces for Tabs

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/maptest/MapTest.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/maptest/MapTest.java
@@ -1,5 +1,10 @@
 package de.hochschuletrier.gdw.ws1415.sandbox.maptest;
 
+import java.util.HashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Gdx;
@@ -33,11 +38,6 @@ import de.hochschuletrier.gdw.ws1415.game.components.PositionComponent;
 import de.hochschuletrier.gdw.ws1415.game.components.SpawnComponent;
 import de.hochschuletrier.gdw.ws1415.sandbox.SandboxGame;
 
-import java.util.HashMap;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  *
  * @author Santo Pfingsten
@@ -52,13 +52,10 @@ public class MapTest extends SandboxGame {
     public static final int GRAVITY = 0;
     public static final int BOX2D_SCALE = 40;
 
-    private final PooledEngine engine = new PooledEngine(
-            GameConstants.ENTITY_POOL_INITIAL_SIZE, GameConstants.ENTITY_POOL_MAX_SIZE,
-            GameConstants.COMPONENT_POOL_INITIAL_SIZE, GameConstants.COMPONENT_POOL_MAX_SIZE
-    );
-    private final PhysixSystem physixSystem = new PhysixSystem(GameConstants.BOX2D_SCALE,
-            GameConstants.VELOCITY_ITERATIONS, GameConstants.POSITION_ITERATIONS, GameConstants.PRIORITY_PHYSIX
-    );
+    private final PooledEngine engine = new PooledEngine(GameConstants.ENTITY_POOL_INITIAL_SIZE, GameConstants.ENTITY_POOL_MAX_SIZE,
+            GameConstants.COMPONENT_POOL_INITIAL_SIZE, GameConstants.COMPONENT_POOL_MAX_SIZE);
+    private final PhysixSystem physixSystem = new PhysixSystem(GameConstants.BOX2D_SCALE, GameConstants.VELOCITY_ITERATIONS,
+            GameConstants.POSITION_ITERATIONS, GameConstants.PRIORITY_PHYSIX);
     private final PhysixDebugRenderSystem physixDebugRenderSystem = new PhysixDebugRenderSystem(GameConstants.PRIORITY_DEBUG_WORLD);
     private final LimitedSmoothCamera camera = new LimitedSmoothCamera();
     private float totalMapWidth, totalMapHeight;
@@ -87,11 +84,10 @@ public class MapTest extends SandboxGame {
         int tileWidth = map.getTileWidth();
         int tileHeight = map.getTileHeight();
         RectangleGenerator generator = new RectangleGenerator();
-        generator.generate(map,
-                (Layer layer, TileInfo info) -> info.getBooleanProperty("blocked", false),
+        generator.generate(map, (Layer layer, TileInfo info) -> info.getBooleanProperty("blocked", false),
                 (Rectangle rect) -> addShape(rect, tileWidth, tileHeight));
-        
-        //Create a SpawnPoint
+
+        // Create a SpawnPoint
         Entity spawn = engine.createEntity();
         PositionComponent spawnPoint = engine.createComponent(PositionComponent.class);
         spawnPoint.x = 200;
@@ -100,7 +96,7 @@ public class MapTest extends SandboxGame {
         spawnflag.reset();
         spawn.add(spawnflag);
         spawn.add(spawnPoint);
-        
+
         engine.addEntity(spawn);
 
         // create a simple player ball
@@ -111,7 +107,7 @@ public class MapTest extends SandboxGame {
         modifyComponent.schedule(() -> {
             playerBody = engine.createComponent(PhysixBodyComponent.class);
             PhysixBodyDef bodyDef = new PhysixBodyDef(BodyType.DynamicBody, physixSystem).position(spawn.getComponent(PositionComponent.class).x,
-            		spawn.getComponent(PositionComponent.class).y).fixedRotation(true);
+                    spawn.getComponent(PositionComponent.class).y).fixedRotation(true);
             playerBody.init(bodyDef, physixSystem, player);
             PhysixFixtureDef fixtureDef = new PhysixFixtureDef(physixSystem).density(5).friction(0.2f).restitution(0.4f).shapeCircle(30);
             playerBody.createFixture(fixtureDef);
@@ -134,7 +130,6 @@ public class MapTest extends SandboxGame {
         float x = rect.x * tileWidth + width / 2;
         float y = rect.y * tileHeight + height / 2;
 
-        
         PhysixBodyDef bodyDef = new PhysixBodyDef(BodyDef.BodyType.StaticBody, physixSystem).position(x, y).fixedRotation(false);
         Body body = physixSystem.getWorld().createBody(bodyDef);
         body.createFixture(new PhysixFixtureDef(physixSystem).density(1).friction(0.5f).shapeBox(width, height));
@@ -150,8 +145,7 @@ public class MapTest extends SandboxGame {
         try {
             return new TiledMap(filename, LayerObject.PolyMode.ABSOLUTE);
         } catch (Exception ex) {
-            throw new IllegalArgumentException(
-                    "Map konnte nicht geladen werden: " + filename);
+            throw new IllegalArgumentException("Map konnte nicht geladen werden: " + filename);
         }
     }
 
@@ -162,11 +156,11 @@ public class MapTest extends SandboxGame {
             mapRenderer.render(0, 0, layer);
         }
         engine.update(delta);
-        
+
         mapRenderer.update(delta);
         camera.update(delta);
 
-        if(playerBody != null) {
+        if (playerBody != null) {
             float speed = 10000.0f;
             float velX = 0, velY = 0;
             if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
