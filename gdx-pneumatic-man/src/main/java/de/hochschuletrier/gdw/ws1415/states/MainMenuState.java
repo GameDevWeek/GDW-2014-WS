6873c857eb9906c7e632e3289a7dd87f083f1dba
Tony Redgrave <mira.lorenz91@gmail.com>
2015-03-18 15:20:22 +0100

    fehlerbehebung

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/states/MainMenuState.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/states/MainMenuState.java
@@ -34,7 +34,7 @@ public class MainMenuState extends BaseGameState {
         menuManager.addLayer(mainMenu);
         
         menuManager.pushPage(mainMenu);
-        menuManager.
+        
 //        menuManager.getStage().setDebugAll(true);
 
         Main.getInstance().addScreenListener(menuManager);
