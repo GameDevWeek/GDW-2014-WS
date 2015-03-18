3412caf6c96c6261f59c2fe791bf5ea937ee2791
Julia <juliaeiden@yahoo.de>
2015-03-18 14:47:02 +0100

    Ingame Menu Class

---
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/menu/IngameMenu.java
@@ -0,0 +1,14 @@
+package de.hochschuletrier.gdw.ws1415.sandbox.menu;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class IngameMenu extends MenuPage
+{
+
+    public IngameMenu(Skin skin, String background)
+    {
+        super(skin, background);
+        // TODO Auto-generated constructor stub
+    }
+
+}
