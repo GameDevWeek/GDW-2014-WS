d4ab5ee9e33a6566915a6f0b3120c77623c9eeb6
Lusito <core@roughael.net>
2015-03-18 18:43:18 +0100

    Hotkeys for changing credits timefactor

--- gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/credits/Credits.java
+++ gdx-test/src/main/java/de/hochschuletrier/gdw/ss14/sandbox/credits/Credits.java
@@ -1,11 +1,14 @@
 package de.hochschuletrier.gdw.ss14.sandbox.credits;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import de.hochschuletrier.gdw.commons.gdx.assets.AnimationExtended;
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
+import de.hochschuletrier.gdw.commons.gdx.input.hotkey.Hotkey;
+import de.hochschuletrier.gdw.commons.gdx.input.hotkey.HotkeyModifier;
 import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
 import de.hochschuletrier.gdw.ss14.Main;
 import de.hochschuletrier.gdw.ss14.sandbox.SandboxGame;
@@ -20,11 +23,26 @@ import org.slf4j.LoggerFactory;
 public class Credits extends SandboxGame implements SceneAnimator.Getter {
 
     private static final Logger logger = LoggerFactory.getLogger(Credits.class);
+    private final Hotkey increaseSpeed = new Hotkey(this::increaseSpeed, Input.Keys.PAGE_UP, HotkeyModifier.CTRL);
+    private final Hotkey decreaseSpeed = new Hotkey(this::decreaseSpeed, Input.Keys.PAGE_DOWN, HotkeyModifier.CTRL);
+    private final Hotkey resetSpeed = new Hotkey(this::resetSpeed, Input.Keys.HOME, HotkeyModifier.CTRL);
     
     private SceneAnimator sceneAnimator;
     private AssetManagerX assetManager;
     
     public Credits() {
+    }
+    
+    private void increaseSpeed() {
+        sceneAnimator.setTimeFactor(Math.min(10.0f, sceneAnimator.getTimeFactor() + 0.1f));
+    }
+    
+    private void decreaseSpeed() {
+        sceneAnimator.setTimeFactor(Math.max(0.0f, sceneAnimator.getTimeFactor() - 0.1f));
+    }
+    
+    private void resetSpeed() {
+        sceneAnimator.setTimeFactor(1.0f);
     }
 
     @Override
@@ -32,6 +50,13 @@ public class Credits extends SandboxGame implements SceneAnimator.Getter {
         this.assetManager = assetManager;
         try {
             sceneAnimator = new SceneAnimator(this, "data/json/credits.json");
+            
+            // If this is a build jar file, disable hotkeys
+            if (!Main.IS_RELEASE) {
+                increaseSpeed.register();
+                decreaseSpeed.register();
+                resetSpeed.register();
+            }
         } catch (Exception ex) {
             logger.error("Error loading credits", ex);
         }
