908685769d17e8b6d729fc9096ac10559d02cd86
Julia <juliaeiden@yahoo.de>
2015-03-18 13:07:55 +0100

    hud überarbeitet

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/hud/Hud.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/hud/Hud.java
@@ -6,7 +6,6 @@ import org.lwjgl.opengl.GL11;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -17,7 +16,9 @@ import com.badlogic.gdx.utils.Timer.Task;
 
 
 
+
 import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
+import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
 import de.hochschuletrier.gdw.ws1415.Main;
 import de.hochschuletrier.gdw.ws1415.sandbox.SandboxGame;
 
@@ -38,7 +39,6 @@ public class Hud extends SandboxGame
     private Stage stage;
     private BitmapFont font;
     
-    private int score = 0;
     private int time = 0;
     
     private Timer timer;
@@ -47,13 +47,13 @@ public class Hud extends SandboxGame
       @Override
       public void init(AssetManagerX assetManager)
       {
-          batch = new SpriteBatch();
+          batch = DrawUtil.batch;
           stage = new Stage();
-          font = new BitmapFont();
+          font = assetManager.getFont("orbitron");
           timer = new Timer();
           
           
-          initMinerDisplay();
+          initMinerDisplay(assetManager);
           initTimeDisplay();
 //          initScoreDisplay();
           
@@ -78,26 +78,21 @@ public class Hud extends SandboxGame
       }
       
       
-      private void initScoreDisplay()
-      {
-          font.setColor(Color.RED);
-//          font.setScale(2, 2);
-          
-      }
       
       
-      private void initMinerDisplay()
+      private void initMinerDisplay(AssetManagerX manager)
       {
           
           MinerLeftListe = new ArrayList<>();
           MinerFoundListe = new ArrayList<>();
           
           //besser: file aus json datei nehmen
-          textureMinerLeft = new Texture(Gdx.files.internal("data/menuImages/KollegeIconNichtGefunden.png"));
-          textureMinerFound = new Texture(Gdx.files.internal("data/menuImages/KollegeIconGefunden.png"));
+//          textureMinerLeft = new Texture(Gdx.files.internal("data/Images/KollegeIconNichtGefunden.png"));
+//          textureMinerFound = new Texture(Gdx.files.internal("data/menuImages/KollegeIconGefunden.png"));          
           
+          textureMinerFound = manager.getTexture("miner_found");
+          textureMinerLeft = manager.getTexture("miner_notfound");
           
-          sprite = new Sprite(textureMinerFound);
           
          // allMiners = game.getMinersLeft();
           allMiners = 5;
@@ -116,55 +111,53 @@ public class Hud extends SandboxGame
     @Override
         public void update(float delta)
       {
-          Gdx.gl.glClearColor(0.5f, 1, 0.5f, 1);
-          Gdx.gl.glClear(GL11.GL_COLOR_BUFFER_BIT);
-   
-          batch.begin();
           Main.getInstance().screenCamera.bind();
           
 //          font.draw(batch, "Score: " + score, 0, 20);
           
-          font.draw(batch, "Time: " + time, stage.getWidth()/2-50, 20);
+          font.draw(batch, "Time: " + time, 300, 200);
           
 //          sprite.draw(batch);
 //          batch.draw(sprite, 200, 200);
           
           
           //test Changing when Miner is Found
-          if(Gdx.input.isKeyPressed(Keys.M))
+          if(Gdx.input.isKeyJustPressed(Keys.M))
           {
               minerFound();
           }
           //test stop Time
-          if(Gdx.input.isKeyPressed(Keys.T))
+          if(Gdx.input.isKeyJustPressed(Keys.P))
           {
               timer.stop();
+              
+          }
+          if(Gdx.input.isKeyJustPressed(Keys.ESCAPE))
+          {
+              timer.start();
+              
           }
           
           
           
-          int x = 10;
+          int x = 5;
           for (Sprite s : MinerLeftListe)
           {
-              batch.draw(s, x, stage.getHeight()-60);
-              x += 60;
+              batch.draw(s, x, 0);
+              x += 70;
           }
 
           for (Sprite s : MinerFoundListe)
           {
-              batch.draw(s, x, stage.getHeight()-60);
-              x += 60;
+              batch.draw(s, x, 0);
+              x += 70;
           }
-
-          
-          batch.end();
         
       }
     
       @Override
       public void dispose()
       {
-          batch.dispose();
           textureMinerFound.dispose();
           textureMinerLeft.dispose();
         
@@ -172,10 +165,6 @@ public class Hud extends SandboxGame
       
       
       
-      public void addScore(int scored)
-      {
-          score += scored;
-      }
       
       public void minerFound()
       {
@@ -184,7 +173,6 @@ public class Hud extends SandboxGame
               MinerLeftListe.remove(MinerLeftListe.size()-1);
           }
           MinerFoundListe.add(new Sprite(textureMinerFound));
-          score += 200;
       }
       
     
