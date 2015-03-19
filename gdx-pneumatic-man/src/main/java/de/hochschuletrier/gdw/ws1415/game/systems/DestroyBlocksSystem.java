dcab8d8c5c12c859e0d027d65ee48543c71b4b9a
oliver <biwero@uni-trier.de>
2015-03-19 17:38:21 +0100

    fixed all errors ... hopefully

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/systems/DestroyBlocksSystem.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/systems/DestroyBlocksSystem.java
@@ -3,6 +3,7 @@ package de.hochschuletrier.gdw.ws1415.game.systems;
 import com.badlogic.ashley.core.*;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.RayCastCallback;
 import de.hochschuletrier.gdw.commons.gdx.physix.components.PhysixBodyComponent;
@@ -28,31 +29,28 @@ public class DestroyBlocksSystem extends IteratingSystem {
         AnimationComponent ani = entity.getComponent(AnimationComponent.class);
         PhysixBodyComponent physix = ComponentMappers.physixBody.get(entity);
 
-        if(ani != null) {
-            // if (ani.animationFinished) {
-            Vector2 p1 = physix.getBody().getPosition();
-            Vector2 p2 = new Vector2(p1).add( Direction.DOWN.toVector2().scl(GameConstants.getTileSizeY()*GameConstants.getTileSizeY()) ); // FIXME MAGIC NUMBER
-            p1.x = EntityCreator.physixSystem.toBox2D(p1.x);    p1.y = EntityCreator.physixSystem.toBox2D(p1.y);
-            p2.x = EntityCreator.physixSystem.toBox2D(p2.x);    p2.y = EntityCreator.physixSystem.toBox2D(p2.y);
 
-            EntityCreator.physixSystem.getWorld().rayCast(new RayCastCallback() {
-                @Override
-                public float reportRayFixture(Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
+        if(ani != null) {
+            //if (ani.animationFinished) {
+                Vector2 p1 = physix.getBody().getPosition();
+                Vector2 p2 = new Vector2(p1).add(Direction.DOWN.toVector2().scl(GameConstants.getTileSizeY() * 1.5f)); // FIXME MAGIC NUMBER
+
+                EntityCreator.physixSystem.getWorld().rayCast((fixture, point, normal, fraction) -> {
                     PhysixBodyComponent bodyComponent = fixture.getUserData() instanceof PhysixBodyComponent ?
-                            (PhysixBodyComponent)fixture.getUserData() : null;
-                    if(bodyComponent != null){
-                        if( ComponentMappers.block.has(bodyComponent.getEntity())
-                                && ComponentMappers.health.has(bodyComponent.getEntity())){
+                            (PhysixBodyComponent) fixture.getUserData() : null;
+                    if (bodyComponent != null) {
+                        if (ComponentMappers.block.has(bodyComponent.getEntity())
+                                && ComponentMappers.health.has(bodyComponent.getEntity())) {
                             HealthComponent healthComponent = ComponentMappers.health.get(bodyComponent.getEntity());
                             healthComponent.DecrementByValueNextFrame += 1;
                             return 0;
                         }
                     }
                     return 1;
-                }
-            }, p1, p2);
-
-        }
+                }, p1, p2);
+                //ani.animationFinished = false;
+            }
+        //}
     }
 }
 
