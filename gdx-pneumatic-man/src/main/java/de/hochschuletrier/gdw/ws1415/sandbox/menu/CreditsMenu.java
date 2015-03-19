48f8c24c921e5a560e94dd6aeae6a567bf8bd659
Jeremy <jeremy1993@freenet.de>
2015-03-19 15:38:31 +0100

    no message

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/menu/CreditsMenu.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/sandbox/menu/CreditsMenu.java
@@ -1,49 +1,64 @@
 package de.hochschuletrier.gdw.ws1415.sandbox.menu;
 
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-
 import de.hochschuletrier.gdw.commons.gdx.assets.AnimationExtended;
-import de.hochschuletrier.gdw.commons.gdx.assets.AssetManagerX;
-import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
-import de.hochschuletrier.gdw.ws1415.Main;
-import de.hochschuletrier.gdw.ws1415.sandbox.SandboxGame;
+import de.hochschuletrier.gdw.commons.gdx.input.hotkey.Hotkey;
+import de.hochschuletrier.gdw.commons.gdx.input.hotkey.HotkeyModifier;
+import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
 import de.hochschuletrier.gdw.commons.gdx.sceneanimator.SceneAnimator;
-
+import de.hochschuletrier.gdw.commons.gdx.sceneanimator.SceneAnimatorActor;
+import de.hochschuletrier.gdw.ws1415.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-public class CreditsMenu extends SandboxGame implements SceneAnimator.Getter {
-
-    public CreditsMenu(Skin skin, String background)
-    {
-    }
+public class CreditsMenu extends MenuPage implements SceneAnimator.Getter {
 
     private static final Logger logger = LoggerFactory.getLogger(CreditsMenu.class);
-    
-    private SceneAnimator sceneAnimator;
-    private AssetManagerX assetManager;
-    
+//    private final Hotkey increaseSpeed = new Hotkey(this::increaseSpeed, Input.Keys.PAGE_UP, HotkeyModifier.CTRL);
+//    private final Hotkey decreaseSpeed = new Hotkey(this::decreaseSpeed, Input.Keys.PAGE_DOWN, HotkeyModifier.CTRL);
+//    private final Hotkey resetSpeed = new Hotkey(this::resetSpeed, Input.Keys.HOME, HotkeyModifier.CTRL);
 
-    @Override
-    public void init(AssetManagerX assetManager) {
-        this.assetManager = assetManager;
+    private SceneAnimator sceneAnimator;
+
+//    private void increaseSpeed() {
+//        sceneAnimator.setTimeFactor(Math.min(10.0f, sceneAnimator.getTimeFactor() + 0.1f));
+//    }
+//
+//    private void decreaseSpeed() {
+//        sceneAnimator.setTimeFactor(Math.max(0.0f, sceneAnimator.getTimeFactor() - 0.1f));
+//    }
+//
+//    private void resetSpeed() {
+//        sceneAnimator.setTimeFactor(1.0f);
+//    }
+
+    public CreditsMenu(Skin skin, MenuManager menuManager) {
+        super(skin, "menu_bg");
+
         try {
             sceneAnimator = new SceneAnimator(this, "data/json/credits.json");
+            addActor(new SceneAnimatorActor(sceneAnimator));
+
+            // If this is a build jar file, disable hotkeys
+            if (!Main.IS_RELEASE) {
+//                increaseSpeed.register();
+//                decreaseSpeed.register();
+//                resetSpeed.register();
+            }
         } catch (Exception ex) {
             logger.error("Error loading credits", ex);
         }
-    }
 
+        addCenteredButton(menuManager.getWidth() - 100, 54, 100, 40, "Zurück", () -> menuManager.popPage());
+    }
 
     @Override
     public BitmapFont getFont(String name) {
-        return assetManager.getFont(name);
+        return skin.getFont(name);
     }
 
     @Override
@@ -54,20 +69,5 @@ public class CreditsMenu extends SandboxGame implements SceneAnimator.Getter {
     @Override
     public Texture getTexture(String name) {
         return assetManager.getTexture(name);
-    }
-    
-    @Override
-    public void dispose() {
-    }
-
-    @Override
-    public void update(float delta) {
-        if(sceneAnimator != null)
-            sceneAnimator.update(delta);
-        
-        Main.getInstance().screenCamera.bind();
-        DrawUtil.fillRect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), Color.DARK_GRAY);
-        if(sceneAnimator != null)
-            sceneAnimator.render();
     }
 }
