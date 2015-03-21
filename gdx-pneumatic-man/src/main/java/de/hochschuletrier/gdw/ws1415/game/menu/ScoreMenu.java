741e89336958e57831236ab55d323d2cd7d77252
meroremo <mero-yui@hotmail.de>
2015-03-21 18:34:45 +0100

    Update

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/menu/ScoreMenu.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/menu/ScoreMenu.java
@@ -10,13 +10,17 @@ import de.hochschuletrier.gdw.ws1415.game.Score;
 public class ScoreMenu extends MenuPage
 {
 
-    private final DecoImage head = new DecoImage(assetManager.getTexture("score_button_active"));
+    private final DecoImage head = new DecoImage(assetManager.getTexture("score_head"));
+    private final DecoImage overlay = new DecoImage(assetManager.getTexture("score_overlay"));
     public Label[] highscoreLabels; 
-
+    private int x_step = 0;
+	private int step = 0;
+	
     public ScoreMenu(Skin skin, MenuManager menuManager)
     {
         super(skin, "background_menu");
-        addImage((int)(Main.WINDOW_WIDTH/2 - 550), 750, (int) head.getWidth(), (int) head.getHeight(), head);
+        addImage((int)(Main.WINDOW_WIDTH/4 + 120), 750, (int) head.getWidth(), (int) head.getHeight(), head);
+        addImage(Main.WINDOW_WIDTH/10 - 200, Main.WINDOW_HEIGHT/2-300, (int) overlay.getWidth(), (int) overlay.getHeight(), overlay);
         addCenteredImage(450, 750, 108, 108, new DecoImage(assetManager.getTexture("back_button")), () -> menuManager.popPage());
         highscoreLabels = new Label[10];
         createHighscoreList();
@@ -40,7 +44,23 @@ public class ScoreMenu extends MenuPage
 
     public void createHighscoreList(){
         for(int i = 0; i <= (Score.highscores.length - 1); i++){
-            highscoreLabels[i] = this.createLabel(Main.WINDOW_WIDTH/2, (Main.WINDOW_HEIGHT/2 + 100) - i*40);
+        	int y_step = 40;
+        	if(i == 3)
+        	{
+        		x_step += 600;
+        		y_step = 0*40;
+        	}
+        	if (i >= 3)
+        	{
+        		this.createLabel(Main.WINDOW_WIDTH/4 + x_step-50, (Main.WINDOW_HEIGHT/2 + 80) - step*y_step - 47).setText("" + (i+1) + ". ");
+        		highscoreLabels[i] = this.createLabel(Main.WINDOW_WIDTH/4 + x_step, (Main.WINDOW_HEIGHT/2 + 80) - step*y_step - 47);
+        		step++;
+        	}
+        	else
+        	{
+        		this.createLabel(Main.WINDOW_WIDTH/4 + x_step-50, (Main.WINDOW_HEIGHT/2) - i*y_step-47).setText("" + (i+1) + ". ");
+        		highscoreLabels[i] = this.createLabel(Main.WINDOW_WIDTH/4 + x_step, (Main.WINDOW_HEIGHT/2) - i*y_step-47);
+        	}
         }
         updateHighscoreList();
     }
