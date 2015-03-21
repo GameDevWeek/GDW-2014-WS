1a1bf31192f00f39e48cd2ed6750078e2c32ce8e
petersdd <petersdorf1992@gmail.com>
2015-03-21 17:03:32 +0100

    Aktuelles Tileset

--- gdx-pneumatic-man/src/main/resources/data/maps/tilesets/background_block.tsx
+++ gdx-pneumatic-man/src/main/resources/data/maps/tilesets/background_block.tsx
@@ -1,39 +1,39 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="background_block" tilewidth="64" tileheight="64" spacing="64" margin="64">
- <image source="destructable_block.png" width="832" height="192"/>
+<tileset name="backgroundBlock" tilewidth="64" tileheight="64" spacing="64" margin="64">
+ <image source="background_block.png" width="832" height="192"/>
  <tile id="0">
   <properties>
-   <property name="Name" value="Block1Background"/>
+   <property name="Name" value="BackgroundBlock1"/>
    <property name="Type" value="Deko"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
-   <property name="Name" value="Block2Background"/>
+   <property name="Name" value="BackgroundBlock2"/>
    <property name="Type" value="Deko"/>
   </properties>
  </tile>
  <tile id="2">
   <properties>
-   <property name="Name" value="Block3Background"/>
+   <property name="Name" value="BackgroundBlock3"/>
    <property name="Type" value="Deko"/>
   </properties>
  </tile>
  <tile id="3">
   <properties>
-   <property name="Name" value="Block4Background"/>
+   <property name="Name" value="BackgroundBlock4"/>
    <property name="Type" value="Deko"/>
   </properties>
  </tile>
  <tile id="4">
   <properties>
-   <property name="Name" value="Block5Background"/>
+   <property name="Name" value="BackgroundBlock5"/>
    <property name="Type" value="Deko"/>
   </properties>
  </tile>
  <tile id="5">
   <properties>
-   <property name="Name" value="Block6Background"/>
+   <property name="Name" value="BackgroundBlock6"/>
    <property name="Type" value="Deko"/>
   </properties>
  </tile>
