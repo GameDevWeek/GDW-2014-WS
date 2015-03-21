41c421e1dc86ef9115f3a925b805c9ff13083628
Lusito <core@roughael.net>
2015-03-21 16:12:46 +0100

    A couple of cvars, hotkeys and fps display

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/systems/SortedRenderSystem.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/systems/SortedRenderSystem.java
@@ -3,13 +3,21 @@ package de.hochschuletrier.gdw.ws1415.game.systems;
 import java.util.Comparator;
 
 import box2dLight.RayHandler;
+import com.badlogic.ashley.core.Engine;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Matrix4;
+import de.hochschuletrier.gdw.commons.devcon.DevConsole;
+import de.hochschuletrier.gdw.commons.devcon.cvar.CVar;
+import de.hochschuletrier.gdw.commons.devcon.cvar.CVarBool;
+import de.hochschuletrier.gdw.commons.devcon.cvar.CVarFloat;
+import de.hochschuletrier.gdw.commons.devcon.cvar.CVarInt;
+import de.hochschuletrier.gdw.commons.devcon.cvar.ICVarListener;
 
 import de.hochschuletrier.gdw.commons.gdx.utils.DrawUtil;
+import de.hochschuletrier.gdw.ws1415.Main;
 import de.hochschuletrier.gdw.ws1415.game.ComponentMappers;
 import de.hochschuletrier.gdw.ws1415.game.GameConstants;
 import de.hochschuletrier.gdw.ws1415.game.components.LayerComponent;
@@ -32,6 +40,11 @@ public class SortedRenderSystem extends SortedFamilyRenderSystem {
 	private LayerComponent currentLayer = null;
 	private CameraSystem cameraSystem;
 	private Matrix4 scaleMatrix = new Matrix4();
+    private final CVarFloat ambientLight = new CVarFloat("light_ambient", 0.5f, 0, 1, 0, "Ambient light value");
+    private final CVarBool culling = new CVarBool("light_culling", true, 0, "Light Culling");
+    private final CVarBool shadows = new CVarBool("light_shadows", true, 0, "Light Shadows");
+    private final CVarInt blur = new CVarInt("light_blur", 2, 0, 3, 0, "Light blur value");
+    private final DevConsole console;
 	
     @SuppressWarnings("unchecked")
 	public SortedRenderSystem(CameraSystem cameraSystem, RayHandler rayHandler, int priority) {
@@ -60,6 +73,50 @@ public class SortedRenderSystem extends SortedFamilyRenderSystem {
         this.rayHandler.setShadows(GameConstants.LIGHT_SHADOW);
         this.rayHandler.useDiffuseLight(GameConstants.LIGHT_DIFFUSE);
         this.cameraSystem = cameraSystem;
+        
+        console = Main.getInstance().console;
+    }
+
+    @Override
+    public void addedToEngine(Engine engine) {
+        super.addedToEngine(engine);
+        
+        addCVar(ambientLight, this::onAmbientLightChange);
+        addCVar(culling, this::onCullingChange);
+        addCVar(blur, this::onBlurChange);
+        addCVar(shadows, this::onShadowsChange);
+    }
+
+    @Override
+    public void removedFromEngine(Engine engine) {
+        super.removedFromEngine(engine);
+        
+        console.unregister(ambientLight);
+        console.unregister(culling);
+        console.unregister(blur);
+        console.unregister(shadows);
+    }
+
+    private void addCVar(CVar cvar, ICVarListener listener) {
+        console.register(cvar);
+        cvar.addListener(listener);
+        listener.modified(cvar);
+    }
+    
+    private void onAmbientLightChange(CVar cvar) {
+        rayHandler.setAmbientLight(ambientLight.get());
+    }
+    
+    private void onCullingChange(CVar cvar) {
+        rayHandler.setCulling(culling.get());
+    }
+    
+    private void onBlurChange(CVar cvar) {
+        rayHandler.setBlurNum(blur.get());
+    }
+    
+    private void onShadowsChange(CVar cvar) {
+        rayHandler.setShadows(shadows.get());
     }
     
     @Deprecated
