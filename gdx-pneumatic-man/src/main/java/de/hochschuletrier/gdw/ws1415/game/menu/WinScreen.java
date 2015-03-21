e3979803676479dd749feba7dc0b1c33c48ef8bf
Julia <juliaeiden@yahoo.de>
2015-03-21 17:28:37 +0100

    added winscreen

---
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/menu/WinScreen.java
@@ -0,0 +1,48 @@
+package de.hochschuletrier.gdw.ws1415.game.menu;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+import de.hochschuletrier.gdw.commons.gdx.menu.MenuManager;
+import de.hochschuletrier.gdw.ws1415.game.menu.MainMenu.Type;
+
+public class WinScreen extends MenuPage
+{
+    
+    private String[] names = { "Santo Pfingsten", "Jerome Jaehnig", "Tobias Meier", "Sandra Weyand", "David Neubauer",
+        "Oliver Biwer", "David Liebemann", "Markus Schmieder", "Sebastian Krause", "Sinan Sagunc", "Tobias Thierbach",
+        "Hermann Hockemeier", "Dennis Grass", "Tobias Rogalski", "Wladimir Kroeker", "Jeremy Merse", "Yasmin Juncker",
+        "Julia Eiden", "Mira Lorenz", "Maximilian Furrer", "Niklas Zorell", "Michael Kramer",  "Reiko Schroth", "Annika Kremer",
+        "Kevin Messmer", "Hannes Klein", "Felix Wishnewsky", "Noah Plingen", "Manuel Neugebauer", "Marc Amann", "Julia Wolf",
+        "Michelle Kaufmann", "Saskia Hohmann", "Hans Boehme", "Lynn Friedrich", "Maxine Hammen", "Tobias Gepp", "Dominik Petersdorf"};
+    private final BitmapFont font_32;
+    private final BitmapFont font_75;
+    private final BitmapFont font_18;
+    
+    
+    public enum Type
+    {
+        MAINMENU,
+        INGAME
+    }
+    
+    public WinScreen(Skin skin, MenuManager menuManager, Type type)
+    {
+        super(skin, "background_menu" );
+        
+        font_32 = assetManager.getFont("orbitron_32");
+        font_75 = assetManager.getFont("orbitron_75");
+        font_18 = assetManager.getFont("orbitron_18");
+        
+        
+        
+         
+        
+        
+        //fuck off!
+        
+        
+        
+    }
+
+}
