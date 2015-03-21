02175b4e6bb3c6f278372ff10cdea6d49fb62477
Hati Eth <hati.eth@gmail.com>
2015-03-21 18:09:21 +0100

    bloecke gehen wieder

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/EntityCreator.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/EntityCreator.java
@@ -1056,7 +1056,7 @@ public class EntityCreator {
      */
     private static void addRenderComponents(Entity entity, TiledMap map, TileInfo info, int tileX, int tileY, PlayMode playMode, boolean start) {
     	TileSet tileset = map.findTileSet(info.globalId);
-    	int frames = tileset.getIntProperty("AnimationFrames", 1); /// default set to 1 from 0 : editet by asset to load bomb
+    	int frames = tileset.getIntProperty("animationFrames", 1); /// default set to 1 from 0 : editet by asset to load bomb
         
     	assert(frames > 1);
 
