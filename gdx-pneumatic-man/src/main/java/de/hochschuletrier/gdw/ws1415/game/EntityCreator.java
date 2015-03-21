8b9cc6ef863eec13a4696d91c538e862359c898e
David-Neubauer <D.Neubauer@hochschule-trier.de>
2015-03-21 17:18:05 +0100

    sprunghöhe der gegner erhöht

--- gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/EntityCreator.java
+++ gdx-pneumatic-man/src/main/java/de/hochschuletrier/gdw/ws1415/game/EntityCreator.java
@@ -248,7 +248,7 @@ public class EntityCreator {
         entity.add(movementComponent);
 
         JumpComponent jumpComponent = engine.createComponent(JumpComponent.class);
-        jumpComponent.jumpSpeed = -600.0f;
+        jumpComponent.jumpSpeed = -1200.0f;
         jumpComponent.restingTime = 0.003f;
         entity.add(jumpComponent);
 
