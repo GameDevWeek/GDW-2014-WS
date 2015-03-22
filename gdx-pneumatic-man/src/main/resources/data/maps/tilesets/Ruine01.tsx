a60bbd9a4c3b0eb48af1e7024f9e403e0b8f3b7b
petersdd <petersdorf1992@gmail.com>
2015-03-22 12:07:01 +0100

    adding new tilesets

---
+++ gdx-pneumatic-man/src/main/resources/data/maps/tilesets/Ruine01.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Ruine01" tilewidth="1001" tileheight="1080">
+ <image source="Ruine01.png" width="1001" height="1080"/>
+ <tile id="0">
+  <properties>
+   <property name="Name" value="Ruine01"/>
+   <property name="Type" value="MidDeko"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ gdx-pneumatic-man/src/main/resources/data/maps/tilesets/Stalagtitten01.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Stalagtitten01" tilewidth="288" tileheight="987">
+ <image source="Stalagtitten01.png" width="864" height="987"/>
+ <tile id="0">
+  <properties>
+   <property name="Name" value="Stalagtit01"/>
+   <property name="Type" value="MidDeko"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Name" value="Stalagtit03"/>
+   <property name="Type" value="MidDeko"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Name" value="Stalagtit04"/>
+   <property name="Type" value="MidDeko"/>
+  </properties>
+ </tile>
+</tileset>
